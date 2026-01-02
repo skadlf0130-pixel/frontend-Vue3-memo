ba74d1e27f5491f732779769b4cbb04c924594a4
1ethon0822 <1ethyjj9240@gmail.com>
2026-01-02 12:33:51 +0900

    storage

--- src/services/StorageService.js
+++ src/services/StorageService.js
@@ -5,6 +5,7 @@
 
 export class StorageService {
     #storageName; 
+    #lastId;
     // 전역변수(속성), #이 붙으면 private(비공개) 외부에서 접근할 수 없는 속성
 
     //생성자, 객체를 생성하려면 꼭 생성자를 호출.
@@ -14,14 +15,33 @@ export class StorageService {
             throw new Error('스토리지 이름을 입력해 주세요.');
         }
         this.#storageName = storageNmae;
+        this.#lastId = 1;
     }
-    //특정 항목 조회
-    getItem(id){
+    // #: 은닉화, 캡슐화(외부에서 호출할수 없지만 내부에서는 호출할수 있다)
+    //스토리지 데이터 조회 p.161
+    #getStorageData(){
         const json = localStorage.getItem(this.#storageName);
-        if(json){
-            const jsonData = JSON.parse(json);
-            jsonData.items.find(item => item.id === id);
-        }
-        return undefined;
+        if(json) {return JSON.parse(json);} //문자열을 객체로 변환 후 리턴
+        return{}; //빈 객체 리턴
     }
+
+    //스토리지 데이터 저장 p.161
+    #saveStorageData(data){
+        const json = JSON.stringify(data); //객체(data)를 json문자열(value)로 변환
+        localStorage.setItem(this.#storageName,json); // value를 localStorage에 저장
+    }
+
+    //신규 항목 추가 p.162
+    addItem(item){
+        const storageData = this.#getStorageData();
+        storageData[this.#lastId++] = item;
+        this.#saveStorageData(storageData);
+    }
+
+    //전체 항목 조회
+    getItems(){
+        return this.#getStorageData();
+    }
+
+
 }
